75f68566a6ea8a1717b19710db1ee1dcf8fcecd8
Тигран Адамян <tigranadaman@MacBook-Air-Tigran.local>
2025-02-17 21:44:35 +0300

    Добавлены Makefile, composer.json и исполняемый файл

---
+++ src/Cli/Greeting.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BrainGames\Cli;
+
+class Greeting
+{
+    public static function sayHello()
+    {
+        \cli\line("Welcome to the Brain Games!");
+        $name = \cli\prompt("May I have your name?");
+        \cli\line("Hello, $name!");
+
+    }
+}
